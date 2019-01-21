f2f2619c9c34c521786051caa4d4eea058375373
William Candillon <wcandillon@gmail.com>
2019-01-21 07:42:15 +0100

    1️⃣

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "parser": "@typescript-eslint/parser",
+  "plugins": ["@typescript-eslint", "react-native"],
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "extends": [
+    "airbnb"
+  ],
+  "rules": {
+    "max-len": [
+      "error",
+      120
+    ],
+    "quotes": [
+      "error",
+      "double",
+      {
+          "avoidEscape": true
+      }
+    ],
+    "indent": [
+      "error",
+      2
+    ]
+  }
+};
