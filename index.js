476a1562753dc2f24ae6c5b099264e12d4d3656f
William Candillon <wcandillon@gmail.com>
2019-02-17 12:24:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,6 @@ module.exports = {
     "react-native/split-platform-components": 2,
     "react-native/no-inline-styles": 0,
     "react-native/no-color-literals": 2,
-    "react-native/no-raw-text": 2,
+    "react-native/no-raw-text": 0,
   }
 };
