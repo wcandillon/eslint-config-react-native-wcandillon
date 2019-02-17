59568f4e063c75d7abe0980f7c3a85eb9ac65115
William Candillon <wcandillon@gmail.com>
2019-02-17 12:26:30 +0100

    Update App.tsx

--- tests/App.tsx
+++ tests/App.tsx
@@ -13,10 +13,11 @@ export default class App extends React.PureComponent {
   }
 }
 
+const white = "#fff";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: white,
     alignItems: "center",
     justifyContent: "center",
   },
