f14ab889328e2965168af9934b41af6bb613f30d
William Candillon <wcandillon@gmail.com>
2019-02-17 12:00:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ module.exports = {
     "react/jsx-filename-extension": ["error", { "extensions": [".tsx"] }],
     "react-native/no-unused-styles": 2,
     "react-native/split-platform-components": 2,
-    "react-native/no-inline-styles": 2,
+    "react-native/no-inline-styles": 0,
     "react-native/no-color-literals": 2,
     "react-native/no-raw-text": 2,
   }
