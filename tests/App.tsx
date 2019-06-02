c7ee6a3b7f25e5000a07482e0fbd60de53eef4df
William Candillon <wcandillon@gmail.com>
2019-06-02 11:58:26 +0200

    feat(💄): Prettier integration (#12)

--- tests/App.tsx
+++ tests/App.tsx
@@ -19,6 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: white,
     alignItems: "center",
-    justifyContent: "center",
-  },
+    justifyContent: "center"
+  }
 });
