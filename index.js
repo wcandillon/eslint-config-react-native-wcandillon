3d23d55256fe70671498dd43e411df399f2e3593
William Candillon <wcandillon@gmail.com>
2019-06-21 06:53:51 +0200

    fix(📏): Update rules (#21)

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ module.exports = {
     "react-native/no-unused-styles": 2,
     "react-native/split-platform-components": 2,
     "react-native/no-inline-styles": 0,
-    "react-native/no-color-literals": 2,
+    "react-native/no-color-literals": 0,
     "react-native/no-raw-text": 0,
-    "import/no-extraneous-dependencies": 0,
+    "import/no-extraneous-dependencies": 2,
     "react-hooks/rules-of-hooks": 2,
     "react-hooks/exhaustive-deps": 2
   }
