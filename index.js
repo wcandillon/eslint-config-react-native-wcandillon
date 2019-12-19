ae62e0139cf32c2eb5eff9a5edbe533510350022
William Candillon <wcandillon@gmail.com>
2019-12-19 05:58:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ module.exports = {
     "react-native/no-color-literals": 0,
     "react-native/no-raw-text": 0,
     "import/no-extraneous-dependencies": 2,
-    "import/extensions": ["error", "never", { "svg": "always" }],
+    "import/extensions": ["error", "never", { "svg": "always", "png": "always", "jpg": "always", "css": "always" }],
     "react-hooks/rules-of-hooks": 2,
     "react-hooks/exhaustive-deps": 2
   }
