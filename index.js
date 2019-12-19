28360c7470b6b3210a0b746b5edaa2b737b55fdd
William Candillon <wcandillon@gmail.com>
2019-12-19 18:02:36 +0100

    fix(🔧): disable react-native/no-raw-text (#44)

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ module.exports = {
     "react-native/split-platform-components": 2,
     "react-native/no-inline-styles": 0,
     "react-native/no-color-literals": 0,
-    "react-native/no-raw-text": 2,
+    "react-native/no-raw-text": 0,
     "import/no-extraneous-dependencies": 2,
     "import/extensions": ["error", "never", { "svg": "always" }],
     "react-hooks/rules-of-hooks": 2,
