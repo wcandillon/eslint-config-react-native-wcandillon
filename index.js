c77929377794fb65420593ff38b169e202d3725a
William Candillon <wcandillon@gmail.com>
2020-01-14 18:18:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   "parser": "@typescript-eslint/parser",
   "plugins": ["@typescript-eslint", "react-native", "eslint-plugin-react-hooks", "known-imports"],
   "settings": {
+    "known-imports/config-file-path": "node_modules/eslint-config-react-native-wcandillon/known-imports.json",
     "import/resolver": {
       "node": {
            "extensions":[
